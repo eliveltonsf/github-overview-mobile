0a45c72bcc5f2e84f3277bd3b7d59762d11e5839
eliveltonsf <elivelton.seven@gmail.com>
2020-07-26 23:25:20 -0300

    :tada: starting-react-native-project

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Hello World!</Text>
+    </View>
+  );
+}
+
+console.log('hello world');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
