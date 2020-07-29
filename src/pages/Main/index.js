57036c431699f52174f07e2caf5d0a06723d32e5
eliveltonsf <elivelton.seven@gmail.com>
2020-07-29 10:11:31 -0300

    :rocket: creating-form-and-style

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container } from './styles';
+import { Container, Form, Input, SubmitButton } from './styles';
+
+Icon.loadFont();
 
 const Main = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#fff" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 };
 
 export default Main;
