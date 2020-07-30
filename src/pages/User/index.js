6e59c1777eaca9df14823489e6edc0dfe50f177d
eliveltonsf <elivelton.seven@gmail.com>
2020-07-29 23:39:07 -0300

    :art: stylizing-user-data

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,23 +1,68 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ActivityIndicator } from 'react-native';
 import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  Avatar,
+  Name,
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+} from './styles';
 
 export default class User extends Component {
   state = {
     stars: [],
+    loading: false,
   };
 
   async componentDidMount() {
     const { route } = this.props;
     const user = route.params.user;
 
+    this.setState({ loading: true });
+
     const response = await api.get(`/users/${user.login}/starred`);
 
-    this.setState({ stars: response.data });
+    this.setState({ stars: response.data, loading: false });
   }
 
   render() {
-    const { stars } = this.state;
-    return <View />;
+    const { route } = this.props;
+    const { stars, loading } = this.state;
+    const user = route.params.user;
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+
+        {loading ? (
+          <ActivityIndicator color="#999" />
+        ) : (
+          <Stars
+            data={stars}
+            keyExtractor={(star) => String(star.id)}
+            renderItem={({ item }) => (
+              <Starred>
+                <OwnerAvatar source={{ uri: item.owner.avatar_url }} />
+                <Info>
+                  <Title>{item.name}</Title>
+                  <Author>{item.owner.login}</Author>
+                </Info>
+              </Starred>
+            )}
+          />
+        )}
+      </Container>
+    );
   }
 }
